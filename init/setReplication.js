9b3de0006f6c4afd7c54cfe53b335401e9ce042d
EinYs <ysunny32@gmail.com>
2021-10-13 10:34:22 +0900

    host update

--- init/setReplication.js
+++ init/setReplication.js
@@ -1,9 +1,9 @@
 // init/ 
 config = { _id: "rs1", 
     members: 
-    [{ _id: 0, host: "host1:27017" }, 
-    { _id: 1, host: "host2:27017" }, 
-    { _id: 2, host: "host3:27017"}] // "arbiterOnly": true 
+    [{ _id: 0, host: "rs0.cocoice.icu:27017" }, 
+    { _id: 1, host: "rs1.cocoice.icu:27017" }, 
+    { _id: 2, host: "rs2.cocoice.icu:27017"}] // "arbiterOnly": true 
 }
 rs.initiate(config);
 rs.conf();
