6b12928d251d1f78e87080dbc0b6284dc3ddf57e
EinYs <ysunny32@gmail.com>
2021-10-14 17:02:18 +0900

    arbiteronly

--- init/setReplication.js
+++ init/setReplication.js
@@ -3,7 +3,7 @@ config = { _id: "rs1",
     members: 
     [{ _id: 0, host: "rs0.cocoice.icu:27017" }, 
     { _id: 1, host: "rs1.cocoice.icu:27017" }, 
-    { _id: 2, host: "rs2.cocoice.icu:27017"}] // "arbiterOnly": true 
+    { _id: 2, host: "rs2.cocoice.icu:27017", "arbiterOnly": true}] // "arbiterOnly": true 
 }
 rs.initiate(config);
 rs.conf();
